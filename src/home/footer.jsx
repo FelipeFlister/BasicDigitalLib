9ad5da8a78bea414930ae4c441398b8f82c5b964
Felipe <felipeflister@gmail.com>
2025-02-24 15:31:32 -0300

    Basic Digital Libray Footer 75%

--- src/home/footer.jsx
+++ src/home/footer.jsx
@@ -3,25 +3,25 @@ import "../assets/css/home/footer.css"
 export function Footer() {
     return (
         <footer>
-            <div>
+            <div id="Credits">
                 <img src="../src/assets/img/Icon.png" alt="" id="Header-Logo" />
-                <span>
+                <span id="Authors">
                     <h4>Basic Digital Libray - Version: 0.1</h4>
                     <ul>
-                        <li>Felipe.F</li>
+                        <a href="https://github.com/FelipeFlister" target="blank"><li>Felipe.F</li></a>
                         <li>João.P</li>
                         <li>Rafael.C</li>
                     </ul>
-                    <q>O Basic Digital Libray tem apenas fins educativos, confira o nosso <a href="../README.md">README</a></q>
+                    <h5>O Basic Digital Libray tem apenas fins educativos, confira o nosso <a href="../README.md">README</a></h5>
                 </span>
             </div>
-            <div>
+            <div id="Copy">
                 <h4>Copyright © Basic Digital Libray ™</h4>
                 <h5>2025</h5>
             </div>
-            <div>
-                <h4>Confira o projeto:</h4>
-                <a href=""><img src="../src/assets/img/github-icon.png" alt="" id="Header-Logo" /></a>
+            <div id="GitHub-Project">
+                <a href="https://github.com/FelipeFlister/BasicDigitalLib" target="blank"><h4>Confira o projeto:</h4></a>
+                <a href="https://github.com/FelipeFlister/BasicDigitalLib" target="blank"><img src="../src/assets/img/github-icon.png" alt="" id="GitHub-Logo" /></a>
             </div>
                 
         </footer>
